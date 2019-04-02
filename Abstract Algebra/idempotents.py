8b6a40e3dd15636df1be30656d10aa15735009c9
Oleg Silkin <o.silkin98@gmail.com>
2019-04-01 20:53:49 -0400

    Makes module far more functional

--- Abstract Algebra/idempotents.py
+++ Abstract Algebra/idempotents.py
@@ -1,20 +1,21 @@
 
-def get_idempotents(n: int) -> list:
+def find_idempotents(n: int) -> list:
     return [a for a in range(n) if a % n == (a**2) % n]
 
+def idempotent_lists(n_start: int, n_end: int, step: int) -> list:
+    assert n_start > 1 and step > 0
+    return [(n, find_idempotents(n)) for n in range(n_start, n_end + 1, step)]
+
+def print_idempotent_info(n: int, idempotents: list):
+    print(f"The following are all idempotent in {n}:")
+    for e in idempotents:
+        print(f"{e}: {e}² = {e**2} = {e} + {e**2 - e} = {e} + {12}*{(e**2 - e)//12}")
+
+def show_idempotents(n_start: int, n_end: int, step: int = 1):
+    idempotents = idempotent_lists(n_start, n_end, step)
+    for mod_n, idems in idempotents:
+        print_idempotent_info(mod_n, idems)
+        print('-'*80)
+
 if __name__ == '__main__':
-    try:
-        n = int(input("Specify the n for congruency ring\nn:  "))
-        idempotents = get_idempotents(n)
-        print(f"The following are all idempotent in {n}:")
-        for e in idempotents:
-            print(f"{e}: {e}² = {e**2} = {e} + {e**2 - e} = {e} + {12}*{(e**2 - e)//12}")
-    except TypeError:
-        from time import sleep
-        print("\WhOOPsIE DAIsy LOokS liKE yOU maDE a MIstAKe. ")
-        sleep(0.6)
-        print("Aww Shucks We'RE not gonna tell you ANYTHING ABOUT THIS ER7ROR")
-        sleep(0.3)
-        print("EVEN IF IT MIGHT BE HELPFUL!!!!!!!!!!!")
-    finally:
-        print("\ndone.")
+    show_idempotents(2, 36)
